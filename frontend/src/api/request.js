f5098dbe64e7b087768be25c0b134df8022b30d6
Ahmuuuuu <18098501156@163.com>
2026-07-11 14:54:03 +0800

    ppt模板选择

--- frontend/src/api/request.js
+++ frontend/src/api/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'http://10.198.169.72:2221',
+  baseURL: 'https://compel-dividable-unfasten.ngrok-free.dev',
   timeout: 300000
 })
 
