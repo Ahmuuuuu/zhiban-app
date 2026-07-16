0f0acc27fa78d2314283e37662a5d53e5f30533b
zhangjinkang-zjk <2136365895@qq.com>
2026-07-16 11:01:00 +0800

    无用文件清除

--- backend/migrations/alter_exam_questions_point_value_to_float.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE exam_questions MODIFY COLUMN point_value FLOAT NULL COMMENT '题目分值(null则按难度自动计算)';
--- backend/migrations/update_node_resource_types.sql
+++
@@ -1,5 +0,0 @@
--- 将存量节点的 resource_types 从旧默认值（2种）更新为新默认值（4种）
--- 只更新恰好等于旧默认值的节点，保留可能的自定义配置
-UPDATE path_nodes
-SET resource_types = '["document", "ppt", "mindmap"]'
-WHERE resource_types = '["document", "image"]';
